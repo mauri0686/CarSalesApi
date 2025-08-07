5423524de2d5fd84d97c7cdd66f63a1c1f422cb9
mauri <mauri0686@gmail.com>
2025-08-07 17:07:45 -0300

    Add properties for CarType and DistributionCenterId in CreateSaleRequest.

--- CarSalesApi.Application/CreateSaleRequest.cs
+++ CarSalesApi.Application/CreateSaleRequest.cs
@@ -7,7 +7,20 @@ namespace CarSalesApi.Application
     /// </summary>
     public class CreateSaleRequest
     {
+        /// <summary>
+        /// Gets or sets the type of car being sold in a sale request.
+        /// This property specifies the category of the car, such as Sedan,
+        /// SUV, Offroad, or Sport.
+        /// </summary>
         public CarType CarType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier of the distribution center associated with the sale.
+        /// </summary>
+        /// <remarks>
+        /// This property is used to associate a sale with a specific distribution center.
+        /// It is an integral part of the sale creation process and relevant for tracking and reporting sales data.
+        /// </remarks>
         public int DistributionCenterId { get; set; }
     }
 }
