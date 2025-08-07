aacc4ecf2d31af952daccd2bf41f81b774cd4168
mauri <mauri0686@gmail.com>
2025-08-07 16:52:07 -0300

    Add new enum CarType with Sedan, SUV, Offroad, Sport options.

---
+++ CarSalesApi.Domain/CarType.cs
@@ -0,0 +1,12 @@
+namespace CarSalesApi.Domain;
+
+/// <summary>
+/// Represents the different types of cars available in the system.
+/// </summary>
+public enum CarType
+{
+    Sedan,
+    SUV,
+    Offroad,
+    Sport
+}
